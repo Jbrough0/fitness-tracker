e3351e650ce85e533ec8f5e00a2a7a17c5d9e4e6
Josh Broughton <spikebroughton@aol.com>
2021-03-01 14:45:03 -0500

    change

--- server.js
+++ server.js
@@ -11,7 +11,17 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+mongoose.connect(
+  process.env.MONGODB_URI ||
+      'mongodb+srv://new-user:Joker231@cluster0.9flal.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+  }
+);
+
 
 const db =require("routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
